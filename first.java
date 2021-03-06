fd4614de5735e3a3b1821dd6a57b03dcb0f18fc2
Sanjay <sanjay.20mis7004@vitap.ac.in>
2021-03-06 13:12:36 +0530

    Minor changes

--- first.java
+++ first.java
@@ -3,9 +3,11 @@ public class First{
 	public static void main(String ar[]){
 		int a=1;
 		int b=2;
+		int c=66;
 		int temp;
-		temp= a+b;
+		temp= a+b+c;
 		System.out.println(temp);
+		System.out.println("63");
+
 	}
 }
-
